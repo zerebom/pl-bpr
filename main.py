d91993001f1a7a562ae65ae24ab1490bcbf75d60
kokoro higuchi <ice.choco.pudding.kokoro@gmail.com>
2021-10-10 12:38:28 +0900

    Change main notebook style -> pyfile style

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
-#%%
-# %load_ext autoreload
-# %autoreload 2
-
-
 from typing import Callable, Optional, TypedDict
 
 import numpy as np
 import pytorch_lightning as pl
 import scipy.sparse as sp
 import torch
-from metric import MRR, Precision, Recall
 from pytorch_lightning.loggers import WandbLogger
 from torch import nn
 from torch.utils.data import DataLoader
@@ -18,33 +12,20 @@ from tqdm import tqdm
 import utils
 from datasets import Interactions, PairwiseInteractions
 from losses import bpr_loss
-from metrics import get_row_indices
+from metrics import MRR, Precision, Recall, get_row_indices
 from models import BPRModule, MFModule
 from pl_modules import MFPLModule
-
-train_data, test_data = utils.get_movielens_train_test_split(implicit=True)
-
-
-class Params(TypedDict):
-    batch_size: Optional[int]
-    num_workers: int
-    n_epochs: Optional[int]
-
-
-params = Params(batch_size=1024, num_workers=0, n_epochs=50)
-
-
-#%%
 
 
 def train(
     train_data: sp.coo.coo_matrix,
     test_data: sp.coo.coo_matrix,
-    model=MFModule,
-    datasets_cls=Interactions,
-    loss: Callable = nn.MSELoss(reduction="sum"),
-    params: Params = params,
+    model,
+    datasets_cls,
+    params: Params,
+    loss: Callable,
 ) -> MFPLModule:
+
     train_ds = datasets_cls(train_data)
     test_ds = datasets_cls(test_data)
 
@@ -76,59 +57,68 @@ def train(
     return pl_module
 
 
-#%%
-mf_module = train(
-    train_data,
-    test_data,
-    model=MFModule,
-    datasets_cls=Interactions,
-    loss=nn.MSELoss(reduction="sum"),
-    params=params,
-)
+def evaluate(model, test_data: sp.csr_matrix):
+    def pred_by_user(user: int, interactions: sp.csr_matrix, model):
+        n_items = interactions.shape[1]
+        items = torch.arange(0, n_items).long()
+        users = torch.ones(n_items).long().fill_(user)
 
-#%%
+        preds = model.predict(users, items).detach().numpy()
+        actuals = get_row_indices(user, interactions)
 
-bpr_module = train(
-    train_data,
-    test_data,
-    model=BPRModule,
-    datasets_cls=PairwiseInteractions,
-    loss=bpr_loss,
-    params=params,
-)
+        if len(actuals) == 0:
+            return None, None
 
-#%%
+        y_test = np.zeros(n_items)
+        y_test[actuals] = 1
 
+        return preds, y_test
 
-def pred_by_user(user: int, interactions: sp.csr_matrix, model):
-    n_items = interactions.shape[1]
-    items = torch.arange(0, n_items).long()
-    users = torch.ones(n_items).long().fill_(user)
+    metrics = [Recall(k=30), Precision(k=30), MRR(k=30)]
 
-    preds = model.predict(users, items).detach().numpy()
-    actuals = get_row_indices(user, interactions)
+    n_users = test_data.shape[0]
+    for user_id in tqdm(range(n_users)):
+        pred, test = pred_by_user(user_id, test_data.tocsr(), model)
+        if pred is None:
+            continue
 
-    if len(actuals) == 0:
-        return None, None
+        for metric in metrics:
+            metric.append(pred, test)
 
-    y_test = np.zeros(n_items)
-    y_test[actuals] = 1
-
-    return preds, y_test
+    return [m.mean() for m in metrics]
 
 
-#%%
+class Params(TypedDict):
+    batch_size: Optional[int]
+    num_workers: int
+    n_epochs: Optional[int]
 
 
-metrics = [Recall(k=30), Precision(k=30), MRR(k=30)]
+def run():
+    train_data, test_data = utils.get_movielens_train_test_split(implicit=True)
+    params = Params(batch_size=1024, num_workers=0, n_epochs=50)
 
-n_users = test_data.shape[0]
-for user_id in tqdm(range(n_users)):
-    pred, test = pred_by_user(user_id, test_data.tocsr(), bpr_module.model)
-    if pred is None:
-        continue
+    mf_module = train(
+        train_data,
+        test_data,
+        model=MFModule,
+        datasets_cls=Interactions,
+        loss=nn.MSELoss(reduction="sum"),
+        params=params,
+    )
 
-    for metric in metrics:
-        metric.append(pred, test)
+    bpr_module = train(
+        train_data,
+        test_data,
+        model=BPRModule,
+        datasets_cls=PairwiseInteractions,
+        loss=bpr_loss,
+        params=params,
+    )
 
-[m.mean() for m in metrics]
+    print(evaluate(mf_module.model, test_data.tocsr()))
+    print(evaluate(bpr_module.model, test_data.tocsr()))
+
+
+if __name__ == "__main__":
+    run()
