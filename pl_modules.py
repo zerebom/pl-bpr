dc7ff15c8c8fbbbe1e331ef97f82323938a9e8c3
kokoro higuchi <ice.choco.pudding.kokoro@gmail.com>
2021-11-22 22:32:01 +0900

    Comment out validation_epoch_end

--- pl_modules.py
+++ pl_modules.py
@@ -55,11 +55,12 @@ class MFPLModule(pl.LightningModule):
         self.log("val_loss", loss, prog_bar=False)
         return {"users": users, "preds": preds, "loss": loss}
 
-    def validation_epoch_end(self, outputs):
-        aucs = []
-        for output in outputs:
-            aucs.append(batch_auc(output["users"], self.csr_mat, self.model))
-        self.log("val_roc", torch.Tensor([np.mean(aucs)]), prog_bar=True)
+    # def validation_epoch_end(self, outputs):
+        # TODO: batch_auc is too slow. We should use a faster metric.
+        # aucs = []
+        # for output in outputs:
+        #     aucs.append(batch_auc(output["users"], self.csr_mat, self.model))
+        # self.log("val_roc", torch.Tensor([np.mean(aucs)]), prog_bar=True)
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(
